16366823f383bfc2b55c6d2e702950ca1294486b
Nate Eizenga <eizengan@gmail.com>
2022-10-30 15:05:00 -0400

    bundle gem lazy_initialized_attributes \ --no-exe \ --coc \ --no-ext \ --mit \ --test=rspec \ --ci=github \ --linter=rubocop

---
+++ lib/lazy_initialized_attributes/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module LazyInitializedAttributes
+  VERSION = "0.1.0"
+end
---
+++ spec/lazy_initialized_attributes_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe LazyInitializedAttributes do
+  it "has a version number" do
+    expect(LazyInitializedAttributes::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
